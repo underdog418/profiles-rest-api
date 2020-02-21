b7f4c19688d596681b38c72cc78dba5d1dfd14c9
Michael <3cfamily418@gmail.com>
2020-02-21 19:42:13 +0800

    just modified __str__ return for testing

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 # these are the two classes need to modify default Django user/admin login
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+
 from django.contrib.auth.models import BaseUserManager
 
 
@@ -12,9 +13,9 @@ class UserProfileManager(BaseUserManager):
         if not email:
             raise ValueError("User must have an email address")
 
-        #make the email letter case insensitive
+        #make the email letter case insensitive a method from BaseUserManager
         email = self.normalize_email(email)
-        # create user model object without the password
+        # create user model object without the password by using .model attribute from BaseuserManager class https://stackoverflow.com/questions/51163088/self-model-in-django-custom-usermanager
         user = self.model(email=email, name=name)
         # store the password not as string but an encripted to hash
         user.set_password(password)
@@ -27,6 +28,7 @@ class UserProfileManager(BaseUserManager):
         user = self.create_user(email, name, password)
         # We did not specify is_superuser because is automatically created by PermissionMixin
         user.is_superuser = True
+        # this method is specified in the UserProfile model
         user.is_staff = True
         user.save(using=self._db)
         return user
@@ -58,4 +60,5 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         """Return string representation of the user"""
-        return self.email
+        return f'{self.email} is {self.name}'
+        #return self.email

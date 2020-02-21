b89b2dd21f6b1e7ecf6fbd9beed0557cadb65225
Michael <3cfamily418@gmail.com>
2020-02-21 14:17:45 +0800

    added user authentication models, manager and migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+# these are the two classes need to modify default Django user/admin login
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError("User must have an email address")
+
+        #make the email letter case insensitive
+        email = self.normalize_email(email)
+        # create user model object without the password
+        user = self.model(email=email, name=name)
+        # store the password not as string but an encripted to hash
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+        def create_superuser(self, email, name, password):
+            """Crate and save a new superuser with given details"""
+            user = self.create_user(email, name, password)
+            # We did not specify is_superuser because is automatically created by PermissionMixin
+            user.is_superuser = True
+            user.is_staff = True
+            user.save(using=self._db)
+            return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users authentication in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # Creates a user profile manager
+    objects = UserProfileManager()
+
+    # Overwriting the default Django username and replace it with USERNAEM_FIELD
+    # by taking user's email instead a user's name
+    USERNAME_FIELD = 'email'
+    # making the name as a requirement field
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Retrieve full name of user"""
+        return self.name
+
+    def get_short_name(self):
+        """Retrieve short name of user"""
+        return self.name
+
+    def __str__(self):
+        """Return string representation of the user"""
+        return self.email
--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+"""beucause we modified the user autherization so we need to point/tell
+Django where to look for the user autherization.
+"""
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
